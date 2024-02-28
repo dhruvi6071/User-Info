8811c332a0af793616699ac8ae93d09c6651f345
dhruvi6071 <dhruvi6071@gmail.com>
2024-02-29 02:46:04 +0530

    alternate-way-of-props

--- user-info/user-info/src/components/User.js
+++ user-info/user-info/src/components/User.js
@@ -1,14 +1,16 @@
 import { Component } from 'react';
 import classes from './User.module.css';
 
-class User{
+//class based component
+class User extends Component{
   render() {
-    return <li className={classes.user}>{props.name}</li>
+    return <li className={classes.user}>{this.props.name}</li>
   }
 }
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+//functional compoenent
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
